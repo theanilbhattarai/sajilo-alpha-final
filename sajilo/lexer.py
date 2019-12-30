ec32889750eb85198e657574f6e28b61556618eb
Harris Kunwar <iamtheparzival@gmail.com>
2019-12-30 21:45:00 +0545

    Small changes lexer

--- sajilo/lexer.py
+++ sajilo/lexer.py
@@ -32,211 +32,154 @@ keywords = {
     'haina': 'NOT',
 }
 
-tokens = list(keywords.values()) + (
-    # Different Types
-    'INT', #Integer - Anka
-    'FLOAT', #Floating point values
-    'STRING', #letters
-    'ID', #identifiers
+tokens = [
+    'KEYWORD',
+    'STMT_END',
+    'EQUALS',
+    'IDENTIFIER',
+    'NUM_INT',
+    'NUM_FLOAT',
+    'LPAREN',
+    'RPAREN',
+    'LBRACK',
+    'RBRACK',
+    'COMMA',
+    'STRING',
+    'NEWLINE',
+    'LSQBRACK',
+    'RSQBRACK',
+    'COLON',
+    'QUESTION_MARK',
 
-    # Operators
     'PLUS',
+    'EXP',
     'MINUS',
     'MUL',
     'DIV',
-    'INT_DIV',
+    'MOD',
 
-    #Comparision Operators
+    'LSHIFT',
+    'RSHIFT',
+    'BIT_AND',
+    'BIT_OR',
+    'BIT_XOR',
+    'BIT_NEG',
+
+    'DOUBLE_PLUS',
+    'DOUBLE_MINUS',
+
+    'PLUS_EQ',
+    'MINUS_EQ',
+    'MUL_EQ',
+    'DIV_EQ',
+    'MOD_EQ',
+    'EXP_EQ',
+
+
+    'TRUE',
+    'FALSE',
+
     'EQ',
-    'DUB_EQ',
-    'NT_EQ',
-    'LT',
+    'NEQ',
     'GT',
+    'GTE',
+    'LT',
+    'LTE',
 
-    #Dividers/ Brackets
-    'LPAREN', #(
-    'RPAREN', #)
-    'LCURLY', # {
-    'RCURLY', # }
+    'ARROW_LTR',
+    'ARROW_RTL'
+] + list(keywords.values())
 
-    # Terminators
-    'COLON', # :
-    'COMMA', #,
-
-    # Signs
-    'DBLT',
-    'BANG',
-
-    #Extras
-    'LINE',
-
-)
-
-
-## LEXER EXPRESSIOŃ #
-
-# Parsing char tokens with regex.
-
-# Operators
+t_COMMA = ','
 t_PLUS = r'\+'
-t_MINUS = r'-'
+t_EXP = r'\*\*'
+t_MINUS = '-'
 t_MUL = r'\*'
 t_DIV = r'/'
-t_INT_DIV = r'//'
-
-# Dividers / Brackets
+t_MOD = '%'
+t_STMT_END = ';'
+t_QUESTION_MARK = r'\?'
+t_EQUALS = '='
+t_ignore_WS = r'\s+'
+t_COLON = ':'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-t_LCURLY = r'{'
-t_RCURLY = r'}'
+t_LBRACK = '{'
+t_RBRACK = '}'
+t_LSQBRACK = r'\['
+t_RSQBRACK = r'\]'
+t_EQ = '=='
+t_NEQ = '!='
+t_GT = '>'
+t_GTE = '>='
+t_LT = '<'
+t_LTE = '<='
+t_ARROW_LTR = '->'
+t_ARROW_RTL = '<-'
+t_ignore_COMMENTS = r'//.+'
+t_PLUS_EQ = r'\+='
+t_MINUS_EQ = r'-='
+t_MUL_EQ = r'\*='
+t_DIV_EQ = r'/='
+t_MOD_EQ = '%='
+t_EXP_EQ = '\*\*='
 
-# Comparision Operators
-t_EQ = r'\='
-t_DUB_EQ = r'=='
-t_NT_EQ = r'!='
-t_LT = r'<'
-t_GT = r'>'
+t_RSHIFT = '>>'
+t_LSHIFT = '<<'
+t_BIT_AND = r'\&'
+t_BIT_OR = r'\|'
+t_BIT_XOR = r'\^'
+t_BIT_NEG = r'~'
 
-#Terminators
-t_COMMA = r','
-t_COLON = r':'
-
-#Signs
-t_DBLT = r'<<'
-t_BANG = r'!'
-t_LINE = r'\|'
-
-
-### LEXING THE TYPES ### 
-
-#Floating values
-def t_FLOAT(tok):
-    #search for the pattern with one point
-    r"""\d+\.\d+"""
-    tok.value = float(tok.value)
-    return tok
-
-# Integer
-def t_INT(tok):
-    #search digits
-    r"""\d+"""
-    tok.value = int(tok.value)
-    return tok
-
-#strings 
-def t_STRING(tok):
-    #pattern match the a-z, A-Z , _ 0-9 , and esp chars
-    r""""[a-zA-Z_ 0-9!?]*\""""
-    tok.value = str(tok.value)[1:1]
-    return tok
+t_DOUBLE_PLUS = r'\+\+'
+t_DOUBLE_MINUS = '--'
 
 
-## Tokenizeres for the keywords
-## Never tokenize the keywords in this way, its always a bad idea.
-
-'''
-#man
-def t_MAN(tok):
-    r"""man"""
-    tok.type = "MAN"
-    return tok
-
-#if/yedi
-def t_IF(tok):
-    r"""yedi"""
-    tok.type = "YEDI"
-    return tok
-
-#else/navaye
-def t_NAVAYE(tok):
-    r"""navaye"""
-    tok.type = "NAVAYE"
-    return tok
-
-# vidhi/method
-def t_VIDHI(tok):
-    r"""vidhi"""
-    tok.type = "VIDHI"
-    return tok
-
-#output/write/print/return
-def t_LEKHA(tok):
-    r"""lekha"""
-    tok.type = "LEKHA"
-    return tok
-
-#read/input/scanf
-def t_PADHA(tok):
-    r"""padha"""
-    tok.type = "PADHA"
-    return tok
-'''
-# unique identifers and keywords
-def t_ID(tok):
-    r"""[a-zA-Z_][a-zA-Z_0-9]*"""
-    if tok.value in keywords:
-        tok.type = tok.value
-    else:
-        tok.type = "ID"
-    return tok
-
-# Tracking the line breaks with regex.
-# Why do this - it will get it difficult to track and parse
-# escape chars later
-def t_newline(tok):
-    r"""\n+"""
-    tok.lexer.lineno += len(tok.value)
-
-
-## Tracking the column numbers
-## Lex.py does not do any automatic column tracking.
-## but id records pos information related to each token with lexpos attribute
-## We can compute column number with a sep step
-## Count backwards until we reach a newline
-
-def find_column(input, token):
-    line_start = input.rfind('\n',0,token.lexpos) + 1
-    return (token.lexpos - line_start) + 1
-
-"""This is only required if the token declartion 
-isn't done
-def t_COMMENT(t):
-    r'\/\/.*'
-    #Not returning value because comments are discarded
+def t_NEWLINE(t):
+    r'\n'
+    t.lexer.lineno += 1
+    t.lexer.linepos = 0
     pass
-"""
-# Ignore tabs and Spaces.
-# NET = Non esstional Token
-t_ignore = ' \t'
-
-# Comments
-# C style comments
-t_ignore_COMMENT = r'\/\/.*' 
 
 
-## Simple Error Handling rules.
-def t_error(tok):
-    err_msg = "Illegal character Error {}".format(tok.value[0])
-    #print the error message
-    print(err_msg)
-    #skip the token
-    tok.lexer.skip(1)
+def t_TRUE(t):
+    'true'
+    t.value = True
+    return t
 
-## Handling the EOF (End of File)
-## The function t_eof is used to handle EOF
-## Integrate during the parsing process
-## when I am actually working with separate files
-"""
-def t_eof(tok):
-    more = raw_input("...")
-    if more:
-        self.lexer.input(more)
-        return self.lexer.token()
-    return None
-"""
 
-#Initialize the Flex method.
+def t_FALSE(t):
+    'false'
+    t.value = False
+    return t
 
+
+def t_IDENTIFIER(t):
+    r'[\$_a-zA-Z]\w*'
+
+    t.type = keywords.get(t.value, t.type)
+
+    return t
+
+
+def t_NUM_FLOAT(t):
+    r'\d*\.\d+'
+    t.value = float(t.value)
+    return t
+
+
+def t_NUM_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_STRING(t):
+    r'"(?:\\"|.)*?"'
+    t.value = bytes(t.value.lstrip('"').rstrip('"'), "utf-8").decode("unicode_escape")
+    return t
+
+
+
+#Build the lexer
 lexer = lex.lex()
-
-
